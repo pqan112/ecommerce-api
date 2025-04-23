4780b2c1b93ccc5f3c49049008df9002bde8c817
AnPQ <anpham1122000@gmail.com>
2025-04-23 21:44:50 +0700

    ECOM-12: Send email with template html

--- ecom/src/shared/services/email.service.ts
+++ ecom/src/shared/services/email.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common'
 import { Resend } from 'resend'
 import envConfig from '../config'
+import fs from 'fs'
+import path from 'path'
+const otpTemplate = fs.readFileSync(path.resolve('src/shared/email-template/otp.html'), 'utf8')
 
 @Injectable()
 export class EmailService {
@@ -11,11 +14,12 @@ export class EmailService {
   }
 
   sendOTP(payload: { email: string; code: string }) {
+    const subject = 'OTP code'
     return this.resend.emails.send({
       from: 'Ecommerce <onboarding@resend.dev>',
-      to: [`${payload.email}`],
+      to: [payload.email],
       subject: 'OTP code',
-      html: `<strong>${payload.code}</strong>`,
+      html: otpTemplate.replaceAll('{{subject}}', subject).replaceAll('{{code}}', payload.code),
     })
   }
 }
